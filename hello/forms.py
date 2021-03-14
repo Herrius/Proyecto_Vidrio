356c6a2745d7b2304342819da82d0406bfc22956
María Isabel <maria@com>
2021-03-14 15:14:40 -0500

    Demo

--- hello/forms.py
+++ hello/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class Valueform(forms.Form):
-    user = forms.CharField(max_length = 100)
+    busqueda = forms.CharField(max_length = 100)
 
 #class RenewBookForm(forms.Form):
 #    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
