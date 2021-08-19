789d4b0b41dbf73abbc4ab0197ab59ec958133b1
María Isabel <maria@com>
2021-08-19 14:04:37 -0500

    Demo

--- hello/chatbotCovid.py
+++ hello/chatbotCovid.py
@@ -3,8 +3,8 @@
 import os
 from aiml import Kernel
 from os import listdir
-from newspaper import Config
-import urllib.request
+#from newspaper import Config
+#import urllib.request
 from bs4 import BeautifulSoup
 import json
 #from nltk import word_tokenize
@@ -88,42 +88,42 @@ def response(user_response):
 
 def apiCamaSusalud(provincia, distrito):
     respuesta_final = ''
-    limite = '100000'
-    url_new = 'http://datos.susalud.gob.pe/api/action/datastore/search.json?resource_id=187105ef-d71b-44e0-a5af-4762c33cefb3&limit=' + limite 
-    rss_text = urllib.request.urlopen(url_new).read().decode('utf8')
-    soup = BeautifulSoup(rss_text,'html.parser')
-    site_json=json.loads(soup.text)
-    #print(site_json)
-    results = site_json['result']
-    for rec in results['records']:
-        #print(i)
-        total_camas = ''    
-        for key in rec.keys():
-            try:
-                a = int(rec[key])
-                if a > 0 and (key.startswith('ZC') or key.startswith('ZNC')):
-                    total_camas = total_camas + ',' + key + str(a)
-                    #print(key, a)
-            except ValueError:
-                it_is = False            
-        if (rec['PROVINCIA'] == provincia) and (rec['DISTRITO'] == distrito) :
-            respuesta_final = respuesta_final + ';' + rec['NOMBRE'] + total_camas
-    print(respuesta_final)
+#    limite = '100000'
+#    url_new = 'http://datos.susalud.gob.pe/api/action/datastore/search.json?resource_id=187105ef-d71b-44e0-a5af-4762c33cefb3&limit=' + limite 
+#    rss_text = urllib.request.urlopen(url_new).read().decode('utf8')
+#    soup = BeautifulSoup(rss_text,'html.parser')
+#    site_json=json.loads(soup.text)
+#    #print(site_json)
+#    results = site_json['result']
+#    for rec in results['records']:
+#        #print(i)
+#        total_camas = ''    
+#        for key in rec.keys():
+#            try:
+#                a = int(rec[key])
+#                if a > 0 and (key.startswith('ZC') or key.startswith('ZNC')):
+#                    total_camas = total_camas + ',' + key + str(a)
+#                    #print(key, a)
+#            except ValueError:
+#                it_is = False            
+#        if (rec['PROVINCIA'] == provincia) and (rec['DISTRITO'] == distrito) :
+#            respuesta_final = respuesta_final + ';' + rec['NOMBRE'] + total_camas
+#    print(respuesta_final)
     return respuesta_final
 
 def apiOxigenoSusalud(provincia, distrito):
     respuesta_final = ''
-    limite = '10'
-    url_new = 'http://datos.susalud.gob.pe/api/action/datastore/search.json?resource_id=b1791142-8fcb-4766-9b8c-b0ee0ffc6dff&limit=' + limite 
-    rss_text = urllib.request.urlopen(url_new).read().decode('utf8')
-    soup = BeautifulSoup(rss_text,'html.parser')
-    site_json=json.loads(soup.text)
-    #print(site_json)
-    results = site_json['result']
-    for i in results['records']:
-        #print(i)
-        if i['PROVINCIA'] == 'Lima':
-            respuesta_final = respuesta_final + ';' + i['NOMBRE']
+#    limite = '10'
+#    url_new = 'http://datos.susalud.gob.pe/api/action/datastore/search.json?resource_id=b1791142-8fcb-4766-9b8c-b0ee0ffc6dff&limit=' + limite 
+#    rss_text = urllib.request.urlopen(url_new).read().decode('utf8')
+#    soup = BeautifulSoup(rss_text,'html.parser')
+#    site_json=json.loads(soup.text)
+#    #print(site_json)
+#    results = site_json['result']
+#    for i in results['records']:
+#        #print(i)
+#        if i['PROVINCIA'] == 'Lima':
+#            respuesta_final = respuesta_final + ';' + i['NOMBRE']
     return respuesta_final
     
 #apiCamaSusalud('LIMA', 'SANTIAGO DE SURCO')
