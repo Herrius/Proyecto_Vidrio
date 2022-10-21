0fbbdf6e90c8e4c3e4b473347404a6f9a54ae4af
María Isabel <maria@com>
2022-10-21 22:24:17 +0200

    Demo

--- hello/clasificacion.py
+++ hello/clasificacion.py
@@ -11,9 +11,10 @@ def cargar_modelo(nombre):
 
 def clasifica(datos):    
     listado = datos.split(sep=';')
+    listado2 = [listado]
     ruta = '/app/hello/modelos/'
     nombre='modelo2.pkl'
     modelo = cargar_modelo(ruta+nombre)
     print(listado)
-    tipo = modelo.predict(listado)
+    tipo = modelo.predict(listado2)
     return tipo
